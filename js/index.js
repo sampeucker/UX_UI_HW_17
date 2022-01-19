4f21c18cb48b4fde56522ad917a342b8af540c73
sampeucker <88971381+sampeucker@users.noreply.github.com>
2022-01-19 15:46:20 +1100

    AYSDJKHASDBAS:KLJasD

---
+++ js/index.js
@@ -0,0 +1,47 @@
+      const cursor = document.querySelector('.cursor');
+
+      document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style", "top: " + (e.pageY - scrollY - 10) + "px; left: " + (e.pageX - 10) + "px")
+      })
+
+
+
+      ScrollReveal().reveal('.ctwm', {  
+         delay :50, 
+         duration: 1000, 
+         reset: true
+        });
+      ScrollReveal().reveal('.taskie', { 
+        delay :100, 
+        duration: 1000,
+        reset: true
+      });
+      ScrollReveal().reveal('.aiatsis', { 
+        delay :150, 
+        duration: 1000,
+        reset: true
+      });
+      ScrollReveal().reveal('.tbu', { 
+        delay :200, 
+        duration: 1000,
+        reset: true
+      });
+      ScrollReveal().reveal('.future', { 
+        delay :250, 
+        duration: 1000,
+        reset: true
+      });
+
+
+
+      ScrollReveal().reveal('.skillCard');
+
+
+      var slideUp = {
+        distance: '50%',
+        origin: 'bottom',
+        opacity: null
+    };
+    
+    ScrollReveal().reveal('.slide-up', slideUp, reveal);
+      
